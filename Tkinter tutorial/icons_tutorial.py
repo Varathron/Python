a07c663a919a64836db1e94994016c977cb6bc15
Varathron <mih4lis@gmail.com>
2020-04-05 17:34:13 +0300

    2

--- Tkinter tutorial/icons_tutorial.py
+++ Tkinter tutorial/icons_tutorial.py
@@ -1,7 +1,6 @@
 from Tkinter import *
 from PIL import ImageTk, Image
 
-
 root = Tk()
 root.title('Tkinter icons tutorial')
 root.iconbitmap('middle.ico')
@@ -10,20 +9,7 @@ my_img = ImageTk.PhotoImage(Image.open("middle.ico"))
 my_label = Label(image=my_img)
 my_label.pack()
 
-
-
-
-
-
-
-
-
-
-
-
-
 button_quit = Button(root, text="Exit Program", command=root.quit)
 button_quit.pack()
-
 
 root.mainloop()
